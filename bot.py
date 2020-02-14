ee0e84a2a37e5fa98dd41c101b6900da1a26e19d
Kevin <kevin.jablonka@epfl.ch>
2020-02-14 17:38:00 +0100

    feat: adding duckduckgo

--- bot.py
+++ bot.py
@@ -20,6 +20,8 @@ Commands:
 - /eln -> open the ELN 
 """
 
+GASPAR = None  # should probably use some kind of session handling for this.
+
 # Handler triggered with the /start command
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
@@ -30,7 +32,8 @@ def send_welcome(message):
 
 
 def get_gaspar(message):
-    gaspar = message.text
+    global GASPAR
+    GASPAR = message.text
     cid = message.chat.id
     bot.send_message(cid, "Thank you, these are the commands I know")
     bot.send_message(cid, bot_commands)
@@ -48,6 +51,36 @@ def step_Set_Topics(message):
     topics = message.text
     response = requests.get("https://source.unsplash.com/random?{0}".format(topics))
     bot.send_photo(message.chat.id, response.content)
+
+
+# send picture from topic
+@bot.message_handler(commands=["search"])
+def handle_search(message):
+    cid = message.chat.id
+    msgQuery = bot.send_message(cid, "Type the query")
+    bot.register_next_step_handler(msgQuery, search)
+
+
+def search(message):
+    # wrapping in try:except to catch unexpected errors, for example: JSONDecodeError
+    try:
+        response = requests.get(
+            "http://api.duckduckgo.com/", params={"q": message.text, "format": "json"}
+        ).json()
+        text = response.get("AbstractText")
+        image_url = response.get("Image")
+        related_topics_text = response.get("RelatedTopics")[0]["Text"]
+        related_topics_icon = response.get("RelatedTopics")[0]["Icon"]["URL"]
+        related_topics_full_link = response.get("RelatedTopics")[0]["FirstURL"]
+    except:
+        bot.send_message(message.from_user.id, "No results")
+        return
+    if not text:
+        bot.send_message(message.from_user.id, related_topics_text)
+        bot.send_message(message.from_user.id, related_topics_full_link)
+        bot.send_photo(message.from_user.id, related_topics_icon)
+    bot.send_message(message.from_user.id, text)
+    bot.send_photo(message.from_user.id, image_url)
 
 
 # configure the webhook for the bot, with the url of the Glitch project
